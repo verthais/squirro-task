ece6c63528b5776f8cc583f5baf505c4a2aae099
Kamil Buchalski <kamil.buchalski@gft.com>
2022-02-24 18:39:16 +0100

    implement blueprint for the api

--- app.py
+++ app.py
@@ -1,13 +1,79 @@
-from flask import Flask
+import uuid
+from flask import Flask, jsonify, make_response, request
+from werkzeug.exceptions import BadRequest
+
 
 def create_app(name: str) -> Flask:
     return Flask(name)
 
 app = create_app('squirro-app')
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+
+@app.route('/text/<text_id>', methods=['GET'])
+def get_text(text_id: str):
+    response = make_response(
+        jsonify({
+            'text': f'text with id {text_id}',
+        }),
+        200,
+    )
+
+    # TODO: retrieve the text from <??>
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+
+@app.route('/text', methods=['POST'])
+def store_text():
+    header = request.headers.get('Content-Type')
+    print(header)
+    if header is None or "application/json" not in header:
+        raise BadRequest('content type must be "application/json"')
+
+    text_id = uuid.uuid4()
+
+    response = make_response(
+        jsonify({
+            'text_id': text_id,
+        }),
+        201,
+    )
+
+    # TODO: store the text in <??>
+    # TODO: store the summarty in <??>
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route('/summary/<text_id>')
+def get_summary(text_id: str):
+    response = make_response(
+        jsonify({
+            "document_id": "example_id",
+            "summary": f"This is the summary of text {text_id}",
+        }),
+        200,
+    )
+
+    # TODO: retrieve the corresponding summary from <??>
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    response = make_response(
+        jsonify({
+            'message': str(e),
+        }),
+        400,
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 
 def main():
