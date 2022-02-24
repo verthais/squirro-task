086e192e1746abf068d252493eab2960b498f03d
Kamil Buchalski <kamil.buchalski@gft.com>
2022-02-24 21:22:46 +0100

    Add database driver and tests

---
+++ database.py
@@ -0,0 +1,140 @@
+import pdb
+import sqlite3
+from schema import Schema
+from uuid import uuid4
+
+class DatabaseError(Exception):
+    "Error for the database events"
+
+class Database():
+    table_name = None
+    schema = None
+    def __init__(self,):
+        self.database_name = None
+        # TODO: make connection share and sustainable
+        self.connection = None
+
+    @classmethod
+    def create(cls, database_name):
+        cmd = "CREATE TABLE {table}({columns})"
+        cols = ','.join(cls.schema.schema.keys())
+        cmd = cmd.format(
+            table=cls.table_name,
+            columns=cols
+        )
+
+        connection = sqlite3.connect(database_name)
+        currsor = connection.cursor()
+
+        currsor.execute(cmd)
+        connection.commit()
+        connection.close()
+
+    def _get_cmd(self, cmd, **kwargs):
+        if kwargs.get('where'):
+            sep = ''
+            condition = 'WHERE '
+            for key, val in kwargs.get('where').items():
+                condition += f'{sep}{key} = "{val}"'
+                sep = ' AND '
+
+        if kwargs.get('col'):
+            cols = ','.join(kwargs.get('col'))
+        else:
+            cols = ','.join(self.schema.schema.keys())
+
+
+        return cmd.format(
+            col = cols ,
+            table = self.table_name,
+            condition = f'{condition}',
+        )
+
+
+    def _put_cmd(self, cmd, id, **kwargs):
+        if kwargs.get('values'):
+            # TODO: support all types
+            values = f'"{id}","' + '","'.join(kwargs.get('values')) + '"'
+        else:
+            raise DatabaseError('no values to insert')
+
+        return cmd.format(
+            table = self.table_name,
+            values = values,
+        )
+
+    def connect(self, database_name: str):
+        self.database_name = database_name
+        self.connection = sqlite3.connect(self.database_name)
+
+    def get(self, **kwargs) -> list:
+        '''
+            kwargs:
+                dry_run: bool - flag to get the raw querry
+                where: dict   - col_name:value pairs to match in where statement
+                col: list     - column names to return values for
+
+            returns:
+                list(tuple(col[,col...]))
+        '''
+        cmd = 'SELECT {col} FROM {table} {condition}'
+        cmd = self._get_cmd(cmd, **kwargs)
+
+        if kwargs.get('dry_run'):
+            return cmd
+
+        cur = self.connection.cursor()
+        data = list(cur.execute(cmd))
+        return data
+
+    def put(self, **kwargs):
+        '''
+            kwargs:
+                dry_run: bool - flag to get the raw querry
+                values: list - in order list of values to insert
+
+            returns:
+                uuid: str    - returns the id of the newly created entity
+        '''
+        id = uuid4()
+        cmd = "INSERT INTO {table} VALUES ({values})"
+        cmd = self._put_cmd(cmd, id, **kwargs)
+
+        if kwargs.get('dry_run'):
+            return cmd
+
+        cur = self.connection.cursor()
+        cur.execute(cmd)
+        self.connection.commit()
+
+        return id
+
+
+class Text(Database):
+    table_name = 'texts'
+    schema = Schema({
+        'id': str,
+        'content': str,
+    })
+    def __init__(self):
+        super().__init__()
+
+
+class Summary(Database):
+    table_name = 'summaries'
+    schema = Schema({
+        'id': str,
+        'text_id': str,
+        'summary': str,
+    })
+    def __init__(self):
+        super().__init__()
+
+def migrate(database_name):
+    tables = [
+        Text,
+        Summary,
+    ]
+
+    for table in tables:
+        table.create(database_name)
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,2 +1,90 @@
-def test_hello_world():
-    assert True == False, "Hello tests!"
+from database import Text, Summary, migrate
+
+
+DATABASE_NAME = 'test.db'
+
+
+def setup_module(_):
+    import os
+    cwd = os.getcwd()
+    db_file = f'{cwd}/{DATABASE_NAME}'
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+    migrate(DATABASE_NAME)
+
+
+def test_text_query():
+    text = Text()
+    text.connect(DATABASE_NAME)
+    query = text.get(
+        dry_run = True,
+        where = {'id': '12345'},
+    )
+
+    assert 'SELECT id,content FROM texts WHERE id = "12345"' == query
+
+
+def test_summary_query():
+    summary = Summary()
+    summary.connect(DATABASE_NAME)
+    query = summary.get(
+        dry_run = True,
+        where = {'id': '12345'},
+    )
+
+    assert 'SELECT id,text_id,summary FROM summaries WHERE id = "12345"' == query
+
+
+def test_text_put():
+    text = Text()
+    text.connect(DATABASE_NAME)
+    query = text.put(
+        dry_run = True,
+        values = ['orginal text to store'],
+    )
+
+    assert 'INSERT INTO texts VALUES ("' in query
+    assert '","orginal text to store")' in query
+
+
+def test_summary_put():
+    summary = Summary()
+    summary.connect(DATABASE_NAME)
+    query = summary.put(
+        dry_run = True,
+        values = ['text_id','summary text to store'],
+    )
+
+    assert 'INSERT INTO summaries VALUES ("' in query
+    assert '","text_id","summary text to store")' in query
+
+
+def test_db_integration():
+    text = Text()
+    text.connect(DATABASE_NAME)
+
+    text_text = 'orginal text to store'
+    text_id = text.put(values=[text_text])
+
+    summary = Summary()
+    summary.connect(DATABASE_NAME)
+    summary_text = [f'{text_id}','summary text']
+    summary_id = summary.put(values=summary_text)
+
+    ret_text = text.get(where={'id': text_id})[0]
+
+    assert ret_text[0] == str(text_id)
+    assert ret_text[1] == text_text
+
+    ret_summary = summary.get(where={'id': summary_id})[0]
+
+    assert ret_summary[0] == str(summary_id)
+    assert ret_summary[1] == str(text_id)
+    assert ret_summary[2] == summary_text[1]
+
+    sum_for_txt = summary.get(where={'text_id': text_id})[0]
+
+    assert sum_for_txt[0] == str(summary_id)
+    assert sum_for_txt[1] == str(text_id)
+    assert sum_for_txt[2] == summary_text[1]
