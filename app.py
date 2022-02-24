0b8f32a603100dd48a1e567cc23a4800ba712a8f
Kamil Buchalski <kamil.buchalski@gft.com>
2022-02-24 21:44:16 +0100

    Implement put/get text, summary from db

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import uuid
 from flask import Flask, jsonify, make_response, request
+from tests.test_database import DATABASE_NAME
 from werkzeug.exceptions import BadRequest, NotFound
 
-from database import Summary, Text, Schema
+from database import Summary, Text
+
+
+DATABASE_NAME = 'squirro-app.db'
+
 
 def create_app(name: str) -> Flask:
+    # TODO: init db if non existing
     return Flask(name)
+
 
 app = create_app('squirro-app')
 
@@ -13,11 +20,11 @@ app = create_app('squirro-app')
 @app.route('/text/<text_id>', methods=['GET'])
 def get_text(text_id: str):
     text = Text()
-    content = text.get({'where':{'id': text_id}})
+    text.connect('squirro.db')
+    content = text.get(where={'id': text_id})
 
     if content is None:
         raise NotFound(f'Not found text with id: {text_id}')
-
 
     response = make_response(
         jsonify({
@@ -30,15 +37,29 @@ def get_text(text_id: str):
     return response
 
 
-
 @app.route('/text', methods=['POST'])
 def store_text():
     header = request.headers.get('Content-Type')
-    print(header)
     if header is None or "application/json" not in header:
         raise BadRequest('content type must be "application/json"')
 
-    text_id = uuid.uuid4()
+    content = request.json.get('text')
+
+    if content is None:
+        raise BadRequest('Request data missing {text}')
+
+    db_text = Text()
+    db_text.connect(DATABASE_NAME)
+    # TODO: compress text before storing
+    text_id = db_text.put(values=[content])
+
+    db_sum = Summary()
+    db_sum.connect(DATABASE_NAME)
+    # TODO: compress summary before storing
+    # TODO: generate summary
+    sumamry = ''
+    sum_id = db_sum.put(values=[text_id, sumamry])
+
 
     response = make_response(
         jsonify({
@@ -47,9 +68,6 @@ def store_text():
         201,
     )
 
-    # TODO: store the text in <??>
-    # TODO: store the summarty in <??>
-
     response.headers["Content-Type"] = "application/json"
     return response
 
@@ -57,7 +75,8 @@ def store_text():
 @app.route('/summary/<text_id>')
 def get_summary(text_id: str):
     summary = Summary()
-    content = summary.get({'where': {'text_id': text_id}})
+    summary.connect('squirro.db')
+    content = summary.get(where = {'text_id': text_id})
 
     if content is None:
         raise NotFound(f'Not found Summary corelated with text with id {text_id}')
