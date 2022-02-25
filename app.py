ed076f69aa342a61d2f8becc2ee198f85ff002d7
Kamil Buchalski <kamil.buchalski@gft.com>
2022-02-25 08:13:08 +0100

    separate init db logic from creata_app

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import logging
 import os
 from summary import initialize
 
-def create_app(name: str) -> Flask:
+
+def init_db():
     logger = logging.getLogger('main:create_app')
     cwd = os.getcwd()
     db_file = f'{cwd}/{DATABASE_NAME}'
-
     if not os.path.exists(db_file):
         logger.info('creating new db: %s', DATABASE_NAME)
         migrate(DATABASE_NAME)
 
+
+def create_app(name: str) -> Flask:
+    init_db()
     initialize()
 
     app = Flask(name)
